05cf0c3d95771eb51662ffb98ba6b71a60a3826e
Diogenes <diogenes.lemos1@gmail.com>
2024-07-16 17:04:36 -0300

    estrutura padrão dos arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-async function copyImg() {
-    const imagemin = (await import('gulp-imagemin')).default;
-    return gulp.src('./src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
-}
-
 function copyIndex() {
     return gulp.src('./src/index.html')
         .pipe(gulp.dest('./'));
@@ -16,28 +9,17 @@ function copyIndex() {
 function distStyle(done) {
     gulp.src('./src/styles/main.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/styles'))
+        .pipe(gulp.dest('./styles'))
         .on('end', done);
 }
 
 function copyJS() {
     return gulp.src('./src/js/*.js')
-        .pipe(gulp.dest('./dist/js'));
-}
-
-function copyIcons() {
-    return gulp.src('./src/icons/*')
-        .pipe(gulp.dest('./dist/icons'));
-}
-
-function copyLogos() {
-    return gulp.src('./src/logos/*')
-        .pipe(gulp.dest('./dist/logos'));
+        .pipe(gulp.dest('./js'));
 }
 
 exports.sass = distStyle;
 exports.javascript = copyJS;
-exports.images = copyImg;
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(distStyle));
@@ -47,9 +29,6 @@ exports.watch = function() {
 
 gulp.task('styles', distStyle);
 gulp.task('js', copyJS);
-gulp.task('icons', copyIcons);
-gulp.task('images', copyImg);
-gulp.task('logos', copyLogos);
 gulp.task('index', copyIndex);
 
-gulp.task('default', gulp.series('index', 'styles', 'js', 'icons', 'images', 'logos'));
+gulp.task('default', gulp.series('index', 'styles', 'js'));
